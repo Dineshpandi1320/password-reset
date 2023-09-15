eac426d7117aebe04926cf5dc306affd4b695091
Dineshpandi1320 <dineshd1307@gmail.com>
2023-09-16 00:43:29 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Routes, Route,  } from "react-router-dom";
+import { PhoneList } from "./PhoneList";
+import { LoginPage } from "./LoginPage";
+import { useNavigate } from "react-router-dom";
+import { SignUpPage } from "./SignUpPage";
+import { ForgetPass } from "./Forget";
+import { VerifyOtp } from "./VerifyOtp";
+import { NewPassword } from "./newPassword";
+import { EmailVerification } from "./EmailVerification";
+import './global.js'; 
 
 function App() {
+  const navigate = useNavigate();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       {/* <nav>
+        <button onClick={() => navigate("/login")}>login</button>
+      </nav>  */}
+      <Routes>
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/login/forgetpassword" element={<ForgetPass />} />
+        <Route path="/verifyotp" element={<VerifyOtp />} />
+        <Route path="/mailverification" element={<EmailVerification />} />
+        <Route path="/setpassword" element={<NewPassword />} />
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/mobiles" element={<PhoneList />} />
+      </Routes>
     </div>
   );
 }
+
+const Home = () => {
+  return <div>welcome to Mobile App</div>;
+};
 
 export default App;
